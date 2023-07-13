151b72e1fcc04b9f73a17c6daaf08d635fd8b6df
Felippe <felipperian365@gmail.com>
2023-07-13 13:45:49 -0300

    Adicionado formulário e função para ler os dados de arquivos excel

--- core/views.py
+++ core/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
-from .models import Kit
+import datetime
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import openpyxl
 # Create your views here.
-class HomeView(CreateView):
-    template_name = 'home.html'
-    allow_empty = True
-    queryset = Kit.objects.all()
-    fields = []
-    # ordering = ['-id']
+def homePage(request):
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+        file = request.FILES['archive']
+
+        wb = openpyxl.load_workbook(file)
+
+        # getting a particular sheet by name out of many sheets
+        sheets = wb.sheetnames
+
+        for sheet in sheets:
+            excel_data = list()
+            worksheet = wb[sheet]
+            
+            for row in worksheet.iter_rows():
+                row_data = list()
+                for cell in row:
+                    row_data.append(str(cell.value))
+                excel_data.append(row_data)
+
+            print(excel_data)
+
+    context = {
+        'form': '',
+    }
+
+    return render(request, 'home.html', context)
