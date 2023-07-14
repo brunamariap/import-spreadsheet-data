343c9a3d44906f6cbec93eb4453baa050ca0dfe2
Bruna Maria <bruna.mariap@outlook.com>
2023-07-14 17:24:26 -0300

    Páginas de listagem e detalhes de kit

--- core/views.py
+++ core/views.py
@@ -5,11 +5,10 @@ from django.urls import reverse
 import openpyxl
 from django.db import transaction
 from .models import Kit
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
 
-
-def homePage(request):
+def home_page_view(request):
 
     # If this is a POST request then process the Form data
     if request.method == 'POST':
@@ -78,63 +77,22 @@ def homePage(request):
             )
 
             new_kit.save()
-        # for index, row in enumerate(excel_data):
-        #     print(excel_data[index])
-        """ 
-            new_kit = Kit.objects.create(
-                type_kit=sheet,
-                identification=excel_data[index][0],
-                code=excel_data[index][1],
-                price=excel_data[index][2],
-                roof=excel_data[index][3],
-                connection=excel_data[index][4],
-
-                quantity_modules=excel_data[index][5],
-                module_model=excel_data[index][6],
-                module_unit_Wp_power=excel_data[index][7],
-
-                max_overload=excel_data[index][8],
-                kWp=excel_data[index][9],
-
-                inverter_1_quantity=excel_data[index][10],
-                inverter_1_model=excel_data[index][11],
-                inverter_2_quantity=excel_data[index][12],
-                inverter_2_model=excel_data[index][13],
-
-                amount_of_red_cables=excel_data[index][14],
-                model_red_cables=excel_data[index][15],
-                amount_of_black_cables=excel_data[index][16],
-                model_black_cables=excel_data[index][17],
-
-                quantity_pairs_connector=excel_data[index][18],
-                connector_pair_model=excel_data[index][19],
-
-                quantity_stringbox=excel_data[index][20],
-                model_stringbox=excel_data[index][21],
-
-                quantity_structure_1=excel_data[index][22],
-                model_structure_1=excel_data[index][23],
-
-                quantity_structure_2=excel_data[index][24],
-                model_structure_2=excel_data[index][25],
-
-                quantity_structure_3=excel_data[index][26],
-                model_structure_3=excel_data[index][27],
-
-                quantity_structure_4=excel_data[index][28],
-                model_structure_4=excel_data[index][29],
-
-                quantity_structure_5=excel_data[index][30],
-                model_structure_5=excel_data[index][31],
-
-                inverter_brand=excel_data[index][32],
-                module_brand=excel_data[index][33],
-            )
-
-            new_kit.save() """
 
     context = {
         'form': '',
     }
 
     return render(request, 'home.html', context)
+
+
+class KitListView(ListView):
+    model = Kit
+    queryset = Kit.objects.all()
+    template_name = 'list-kits.html'
+    paginate_by = 15
+
+
+class KitDetailsView(DetailView):
+    model = Kit
+    allow_empty = True
+    template_name = 'kit-details.html'
